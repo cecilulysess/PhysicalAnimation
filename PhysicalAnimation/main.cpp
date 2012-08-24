3bfc219f45a6f24601ae7e610e3833296fdbfeab
Julian Wu <julian@Julians-MacBook-Air.local>
2012-08-23 22:34:58 -0400

    Initial Commit

---
+++ PhysicalAnimation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PhysicalAnimation
+//
+//  Created by Julian Wu on 8/23/12.
+//  Copyright (c) 2012 Julian Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
